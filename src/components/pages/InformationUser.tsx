0222f07b5f20afa3228019e053a40b85d50d909a
Kyle <kyle91@namtech.au>
2020-09-05 15:51:06 +0700

    done get data

--- src/components/pages/InformationUser.tsx
+++ src/components/pages/InformationUser.tsx
@@ -1,25 +1,36 @@
 import React, { Component } from "react";
-import {connect} from "react-redux"
+import { connect } from "react-redux"
 import { getInfoUserLogin } from "../../store/actions/authAction";
 
 class InformationUser extends Component {
-  async componentDidMount() {
-    await getInfoUserLogin();
-    console.log("test from componentDidMount ");
-    console.log("test from getInfoUserLogin ", getInfoUserLogin);
-  }
-  render() {
-     console.log("test from render InformationUser");
-     
-    return (
-      <div className="container">
-        <div className="col-8">
-            hello world
-        </div>
-        <div className="col-4"></div>
-      </div>
-    );
-  }
+    state = {Account: {
+        FullName: ""
+    }};
+    
+    async componentDidMount() {
+        var account = await getInfoUserLogin();
+        this.setState({Account: account.Data.Account});
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-4">
+                        <h3>Name</h3>
+                        <p>phone</p>
+                        <p>location</p>
+                        <p>DOB</p>
+                    </div>
+                    <div className="col-8">
+                        <form>
+                            <label>Name</label>
+                            <input type="text" placeholder="Name" value={this.state.Account.FullName} />
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 // const mapDispatchtoProps = (dispatch: any) => {
