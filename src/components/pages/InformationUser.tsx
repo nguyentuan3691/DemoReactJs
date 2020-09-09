977dafc6b41fc62b07ae39cc6331baa70420e521
QuangKadata <willyacegaming@gmail.com>
2020-09-09 13:45:09 +0700

    commit v2

--- src/components/pages/InformationUser.tsx
+++ src/components/pages/InformationUser.tsx
@@ -1,173 +1,167 @@
 import React, { Component } from "react";
-import { getInfoUserLogin, updateUser, post_image } from "../../store/actions/authAction";
+import {
+  getInfoUserLogin,
+  updateUser,
+  post_image,
+} from "../../store/actions/authAction";
 import ChangePassword from "./ChangePassword";
-// import {connect} from "react-redux";
 
-
-let moment = require('moment');
+let moment = require("moment");
 
 class InformationUser extends Component {
-    props: any = this.props;
-    state = {
-        url: null,
-        Data: {
-            Id: "",
-            FullName: "",
-            AccountType: "",
-            Birthday: "",
-            Address: "",
-            AvatarUrl: "",
-            PhoneNumber: "",
-            Gender: "",
-            Email: "",
-            Password: "",
-        },
+  props: any = this.props;
+  state = {
+    Data: {
+      Id: "",
+      FullName: "",
+      AccountType: "",
+      Birthday: "",
+      Address: "",
+      AvatarUrl: "",
+      PhoneNumber: "",
+      Gender: "",
+      Email: "",
+      Password: "",
+    },
+  };
+
+  showDOB = () => {
+    return moment(this.state.Data.Birthday).format("DD/MM/YYYY");
+  };
+
+  showGender = () => {
+    return this.state.Data.Gender ? "Male" : "Female";
+  };
+
+  onFileChange = () => {
+    let dataImage = {
+      AvatarUrl: this.state.Data.AvatarUrl,
     };
+    return post_image(dataImage);
+    // const formData = new FormData();
+    // let file = dataImage.target.files;
+    // formData.append("image", file,blob);
+    // let url = URL?.createObjectURL(file);
+    // this.state.Data.AvatarUrl = this.state.Data.ImageUrl;
+  };
 
-    showDOB = () => {
-        return moment(this.state.Data.Birthday).format("DD/MM/YYYY");
-    }
-   
-    showGender = () => {
-        return this.state.Data.Gender ? "Male" : "Female";
-    }
+  loadImage = (): void => {
+      window.open(this.state.Data.AvatarUrl, "")
+  }
+  
+  handleChange = (e: any) => {
+    let state: any = this.state;
+    let data: any = state.Data;
+    data[e.target.name] = e.target.value;
+    this.setState({
+      state,
+    });
+  };
 
+  handleSubmit = (e: any) => {
+    e.preventDefault();
+    this.onFileChange();
+    this.updateUserData();
 
-    async GetImage = () =>{
+    this.showDOB();
+    
+  };
 
-    }
-    onFileChange = (e:any) =>{
-        const formData = new FormData();
-      let AvatarUrl = e.target.files[0];
-      // let limit = 1024 * 1024 * 2;
-      // if (file["size"] > limit) {
-      //   Swal({
-      //     type: "error",
-      //     title: "Oops...",
-      //     text: "You are uploading a large file"
-      //   });
-      //   return false;
-      // }
-      formData.append("image", AvatarUrl);
-      this.
-      //Tranfer data to get
-      AvatarUrl = this.state.Data.AvatarUrl;
-    //   this.state.Data.AvatarUrl = URL?.createObjectURL(AvatarUrl);
-    }
-    handleChange = (e: any) => {
-        let state: any = this.state;
-        let data: any = state.Data;
-        data[e.target.name] = e.target.value;
-        this.setState({
-            state
-        });
-    }
+  updateUserData = () => {
+    let dataUser = {
+      Id: this.state.Data.Id,
+      PhoneNumber: this.state.Data.PhoneNumber,
+      Email: this.state.Data.Email,
+      FullName: this.state.Data.FullName,
+      AvatarUrl: this.state.Data.AvatarUrl,
+      Birthday: moment(this.state.Data.Birthday).format("DD/MM/YYYY"),
+      Address: this.state.Data.Address,
+    };
+    return updateUser(dataUser);
+  };
 
-    handleSubmit = (e: any) => {
-        e.preventDefault();
-        this.updateUserData()
-        
-        this.showDOB()
-    }
+  async componentDidMount() {
+      let account = await getInfoUserLogin();
+      console.log(account);
+      this.setState({
+        Data: account.Data.Account,
+      });
+    // let data = this.state.Data.AvatarUrl;
+    // let url = await post_image(data);
+  }
 
-    updateUserData = () => {
-        let dataUser = {
-            Id: this.state.Data.Id,
-            PhoneNumber: this.state.Data.PhoneNumber,
-            Email: this.state.Data.Email,
-            FullName: this.state.Data.FullName,
-            AvatarUrl: this.state.Data.AvatarUrl,
-            Birthday: moment(this.state.Data.Birthday).format("DD/MM/YYYY"),
-            Address: this.state.Data.Address,
-        }
-        return updateUser(dataUser)
-    }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <img src="this.state.Data.AvatarUrl" className="Avatar-Profile" alt="avatar" />
 
-    async componentDidMount() {
-        let data = this.state.Data.AvatarUrl;
-        let account = await getInfoUserLogin();
-        this.setState({
-            Data: account.Data.Account,
-            
-        });
-        let url = await post_image(data);
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-4">
-                        <img
-                            src={this.state.Data.AvatarUrl}
-                            className="Avatar-Profile"
-                            alt="avatar"
-                        />
-                       
-                        <h3 className="font-weight-bold">{this.state.Data.FullName}</h3>
-                        <p className="text-muted font-weight-bold">{this.state.Data.AccountType}</p>
-                        <p className="font-weight-bold ">{this.state.Data.PhoneNumber}</p>
-                        <p className="font-weight-bold ">{this.showGender()}</p>
-                        <p className="font-weight-bold ">{this.showDOB()}</p>
-                        <p className="font-weight-bold ">{this.state.Data.Address}</p>
-                        <p className="font-weight-bold ">{this.state.Data.Email}</p>
-                        <hr />
-                        <ChangePassword />
-                        
-                    </div>
-                    <div className="col-8">
-                        <form onChange={(e) => this.handleChange(e)} onSubmit={(e) => this.handleSubmit(e)}>
-                            
-                            <button
-                                type="submit"
-                                className="btn btn-info"
-                            >
-                                Save
-                            </button>
-                            <hr />
-                            <input type="file" name="profile_avatar"  accept=".png, .jpg, .jpeg"  onChange={(e) => this.onFileChange(e)}/>
-                            <div className="form-group ">
-                                <label className="float-left">Full Name</label>
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    name="FullName"
-                                    defaultValue={this.state.Data.FullName}
-                                />
-                            </div>
-                            <div className="form-group ">
-                                <label className="float-left">Date of Birth</label>
-                                <input
-                                    type="date"
-                                    className="form-control"
-                                    name="Birthday"
-                                    defaultValue={this.showDOB()}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label className="float-left">Email</label>
-                                <input
-                                    type="email"
-                                    className="form-control"
-                                    name="Email"
-                                    defaultValue={this.state.Data.Email}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label className="float-left">Address</label>
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    name="Address"
-                                    defaultValue={this.state.Data.Address}
-                                />
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+            <h3 className="font-weight-bold">{this.state.Data.FullName}</h3>
+            <p className="text-muted font-weight-bold">
+              {this.state.Data.AccountType}
+            </p>
+            <p className="font-weight-bold ">{this.state.Data.PhoneNumber}</p>
+            <p className="font-weight-bold ">{this.showGender()}</p>
+            <p className="font-weight-bold ">{this.showDOB()}</p>
+            <p className="font-weight-bold ">{this.state.Data.Address}</p>
+            <p className="font-weight-bold ">{this.state.Data.Email}</p>
+            <hr />
+            <ChangePassword />
+          </div>
+          <div className="col-8">
+            <form
+              onChange={(e) => this.handleChange(e)}
+              onSubmit={(e) => this.handleSubmit(e)}
+            >
+              <button type="submit" className="btn btn-info">
+                Save
+              </button>
+              <hr />
+              {/*  */}
+              <input type="file" name="AvatarUrl" accept=".png, .jpg, .jpeg" />
+              <div className="form-group ">
+                <label className="float-left">Full Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="FullName"
+                  defaultValue={this.state.Data.FullName}
+                />
+              </div>
+              <div className="form-group ">
+                <label className="float-left">Date of Birth</label>
+                <input
+                  type="date"
+                  className="form-control"
+                  name="Birthday"
+                  defaultValue={this.showDOB()}
+                />
+              </div>
+              <div className="form-group">
+                <label className="float-left">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  name="Email"
+                  defaultValue={this.state.Data.Email}
+                />
+              </div>
+              <div className="form-group">
+                <label className="float-left">Address</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="Address"
+                  defaultValue={this.state.Data.Address}
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default InformationUser;
--- src/store/actions/authAction.tsx
+++ src/store/actions/authAction.tsx
@@ -1,54 +1,57 @@
 import httpService from "../../core/httpService";
 import { saveToken } from "../../core/jwt.service";
 
-
 export const loginUser = (formValues: any) => {
   return httpService
     .post("Authentications.Accounts", "Login", formValues)
     .then((response) => {
       if (response.Code === 0) {
         saveToken(response.Data.SessionInfo.Token);
-        window.location.assign('/home')
+        window.location.assign("/home");
       }
       return response;
     });
 };
 
 export const getInfoUserLogin = () => {
-  return httpService
-    .get("Authentications.Accounts", "Me").then(response => { return response });
+  return httpService.get("Authentications.Accounts", "Me").then((response) => {
+    return response;
+  });
 };
 
 export const updateUser = (dataUser: any) => {
   return httpService
     .post("Authentications.Accounts", "Update", dataUser)
-    .then(response => {
+    .then((response) => {
       if (dataUser === {}) {
         saveToken(response.Data.SessionInfo.Token);
         localStorage.setItem("dataUser", JSON.stringify(dataUser));
       }
-      return response
-    })
-};
-  export const post_image = (data:any) =>{
-    return httpService.post_file(
-      "HubCommon.FileUpload",
-      "UploadImage",
-      data
-    ).then(response  => { 
-      localStorage.setItem("data", JSON.stringify(data));
       return response;
     });
-  }
+};
+export const post_image = (dataImage: any) => {
+  return httpService
+    .post_file("HubCommon.FileUpload", "UploadImage", dataImage)
+    .then(response => {
+      if (dataImage === {}) {
+        saveToken(response.Data.SessionInfo.Token);
+        localStorage.setItem("dataImage", JSON.stringify(dataImage));
+      }
+      return response;
+    });
+};
 export const changePassword = (formChangePassword: any) => {
   return httpService
-  .post("Authentications.Accounts", "ChangePassword", formChangePassword)
-  .then(response => {
-    if (formChangePassword === {}) {
-      saveToken(response.Data.SessionInfo.Token);
-      localStorage.setItem("formChangePassword", JSON.stringify(formChangePassword));
-    }
-    return response
-  })
-}
-
+    .post("Authentications.Accounts", "ChangePassword", formChangePassword)
+    .then((response) => {
+      if (formChangePassword === {}) {
+        saveToken(response.Data.SessionInfo.Token);
+        localStorage.setItem(
+          "formChangePassword",
+          JSON.stringify(formChangePassword)
+        );
+      }
+      return response;
+    });
+};
